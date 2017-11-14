705112b03a6f10f99c3ff664efc4acbc93fd5492
Owen Magri <a106897@mcast.org.mt>
2017-11-14 10:47:49 +0100

    - Fixed mouse position from pixels to game units - set the ball to be locked to the paddle position - once user clicks mouse button, ball flies up to start the game - created surrounding border

--- Block Breaker/Assets/Scripts/LevelManager.cs
+++ Block Breaker/Assets/Scripts/LevelManager.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
+
 
 	// Use this for initialization
 	void Start () {
