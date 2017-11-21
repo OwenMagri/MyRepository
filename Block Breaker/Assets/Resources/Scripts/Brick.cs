24f5fd5dc50dff549e0729e9d247bb8383f793b5
Owen Magri <a106897@mcast.org.mt>
2017-11-21 12:18:24 +0100

    --Fixed audio clip issue by loading the clip from the script --Checked with which object the ball was colliding --Created a tag for breakable bricks --Counted total number of breakable bricks upon load --Used the breakable brick count to know when to load next level

---
+++ Block Breaker/Assets/Resources/Scripts/Brick.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class Brick : MonoBehaviour {
+
+    public static int breakableCount = 0;
+
+    public int maxHits;
+    AudioClip crack;
+
+    int timesHit;
+
+    LevelManager levelManager = new LevelManager();
+
+	// Use this for initialization
+    void Start () {
+
+        breakableCount++;
+        print(breakableCount);
+
+        crack = Resources.Load("Sound/crack", typeof(AudioClip)) as AudioClip;
+        timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        timesHit++;
+        //print(gameObject.name+": "+timesHit);
+
+        if (timesHit >= maxHits)
+        {
+            //TestWin();
+            AudioSource.PlayClipAtPoint(crack, this.transform.position);
+            breakableCount--;
+            print(breakableCount);
+            levelManager.BrickDestroyed();
+            Destroy(gameObject);
+        }
+    }
+    
+    void TestWin()
+    {
+        levelManager.LoadNextScene();
+    }
+
+    
+}
